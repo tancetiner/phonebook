a8fc97e2d91e9e1dc075ba1dfde8543b5f0ebff9
Tan Çetiner <cetinertan13@gmail.com>
2022-03-10 12:00:02 +0200

    exercise 3.4

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
 
 let persons = [
   {
@@ -29,6 +32,10 @@ let persons = [
   },
 ];
 
+const generateId = () => {
+  return parseInt(Math.random() * 1000000);
+};
+
 app.get("/api/persons", (request, response) => {
   response.json(persons);
 });
@@ -49,6 +56,26 @@ app.delete("/api/persons/:id", (request, response) => {
   persons = persons.filter((person) => person.id !== id);
 
   response.status(204).end();
+});
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  if (!body) {
+    return response.status(400).json({
+      error: "No content in request",
+    });
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  };
+
+  persons = persons.concat(person);
+
+  response.json(person);
 });
 
 const PORT = 3001;
